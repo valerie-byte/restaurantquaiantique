6d8ea303435f56b6d4ead8abd969a0b593c90d6f
valerie-byte <vn1.thouvenot@gmail.com>
2024-05-27 15:58:05 +0200

    finalisation page galerie

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -1,9 +1,10 @@
 // importe le fichier route.js dans le même dossier
 import Route from "./route.js";
 
-//Définir ici vos routes
+//Définir ici vos routes liste des pages
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
+    new Route("/galerie", "La Galerie", "./pages/galerie.html"),
 
 ];
 

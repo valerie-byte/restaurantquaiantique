295f7c9cdaf60526f5f6bd0ef020334b33000bc6
valerie-byte <vn1.thouvenot@gmail.com>
2024-05-27 16:12:19 +0200

    page de connexion sigin

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -1,10 +1,11 @@
 // importe le fichier route.js dans le même dossier
 import Route from "./route.js";
 
-//Définir ici vos routes liste des pages
+//Définir ici vos routes liste des pages /nomdelapage, et chemein d'accès
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La Galerie", "./pages/galerie.html"),
+    new Route("/signin", "connexion", "./pages/signin.html"),
 
 ];
 

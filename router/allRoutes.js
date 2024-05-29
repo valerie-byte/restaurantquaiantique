fca929bc2a225a0a281376e2bc656ce9044db90a
valerie-byte <vn1.thouvenot@gmail.com>
2024-05-29 08:12:15 +0200

    page deconnexion

--- router/allRoutes.js
+++ router/allRoutes.js
@@ -1,11 +1,13 @@
 // importe le fichier route.js dans le même dossier
 import Route from "./route.js";
 
-//Définir ici vos routes liste des pages /nomdelapage, et chemein d'accès
+//Définir ici vos routes liste des pages /nomdelapage, titre et chemein d'accès
+// href = /nompage donc on ouvre le lien correspondant, rech dans le tableau
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La Galerie", "./pages/galerie.html"),
     new Route("/signin", "connexion", "./pages/signin.html"),
+    new Route("/signup", "inscription", "./pages/signup.html"),
 
 ];
 

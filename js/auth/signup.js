9debbc5b0c46aa9c798f4470023f8a3262520029
valerie-byte <vn1.thouvenot@gmail.com>
2024-06-06 08:26:57 +0200

    validation et verif nom prenom et mail

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -5,6 +5,7 @@ const inputPrenom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidatePassword = document.getElementById("ValidatePasswordInput");
+const btnvalidation = document.getElementById("btn-validation-inscription");
 
 // type d'évènement keyup sur touche relachée
 inputNom.addEventListener("keyup",validateForm);
@@ -15,8 +16,34 @@ inputValidatePassword.addEventListener("keyup",validateForm);
 
 // vérifie nom et prénom
 function validateForm() {
-    validateRequired(inputNom);
-    validateRequired(inputPrenom);
+    const nomOk = validateRequired(inputNom);
+    const prenomOk = validateRequired(inputPrenom);
+    const mailOk = validateMail(inputMail);
+
+    if(nomOk && prenomOk && mailOk ) {
+        btnvalidation.disabled = false;
+    } else {
+        btnvalidation.disabled = true;
+    }
+
+}
+
+// fonction pour vérifier si le formalisme du mail est respecté
+function validateMail(input){
+    //Définir mon regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    // match regex bien présent dans le champ à vérifier
+    if(mailUser.match(emailRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid"); 
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
+        return false;
+    }
 }
 
 // fonction quand on met du texte dans le champ nom celui-ci devient vert, si plus de charactère celui ci devient rouge
@@ -25,9 +52,11 @@ function validateRequired (input) {
         // c'est pas ok
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
     } else {
         // c'est  ok
         input.classList.add("is-invalid");
         input.classList.remove("is-valid");
+        return false;
     }
 }

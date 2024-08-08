a0eb4a9f4f7436f8af6d19217bf1f90a6da4321e
valerie-byte <vn1.thouvenot@gmail.com>
2024-08-08 13:30:32 +0200

    add fonction getinfouser

--- js/script.js
+++ js/script.js
@@ -119,3 +119,31 @@ tempHtml.textContent = text;
     // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
 return tempHtml.innerHTML;
 }
+
+function getInfosUser(){
+    // console.log("récupération info utilisateur"); test de la fonction
+    let myHeaders = new Headers();
+    myHeaders.append("X-AUTH-TOKEN", getToken());
+
+    let requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(apiUrl+"account/me", requestOptions)
+    .then(response =>{
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            console.log("Impossible de récupérer les informations utilisateur");
+        }
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(error =>{
+        console.error("erreur lors de la récupération des données utilisateur", error);
+    });
+}
